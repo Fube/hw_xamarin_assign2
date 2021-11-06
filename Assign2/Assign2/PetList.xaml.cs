ce0aa4758e202eb9e39cd271d80ee573211e34f6
Fube <fubeitch@gmail.com>
2021-11-06 16:41:52 -0400

    Added double click to edit pet

--- Assign2/Assign2/PetList.xaml.cs
+++ Assign2/Assign2/PetList.xaml.cs
@@ -20,12 +20,26 @@ namespace Assign2
             var pets = await App.Pets.GetAsync();
             Pets.ItemsSource = pets.Select(n => new
             {
+                OwnerID = n.OwnerID,
                 Name = n.Name,
                 Type = n.Type,
                 ImagePath = $"{n.Type.ToLower()}.jpg"
             });
         }
 
+        private async void HandleDoubleTap(object sender, EventArgs e)
+        {
+            var bindable = (BindableObject)sender;
+            dynamic context = bindable.BindingContext;
+            int ownerId = context.OwnerID;
+
+            if(!App.Principal.IsAdmin && App.Principal.ID != ownerId)
+            {
+                await DisplayAlert("Unauthorized", "You are not allowed to modify this pet", "Ok");
+                return;
+            }
+            // TODO: Add pet edit nav push
+        }
     }
 }
 
